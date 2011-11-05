7e526d9058a5a7f726979210deb062dd7105a9ec
jimmytheleaf <jim.fingal@gmail.com>
2011-11-05 14:13:45 -0400

    tweaking parameter helper

--- rovi.py
+++ rovi.py
@@ -8,38 +8,33 @@ from secrets import apikey, sign
 HOSTNAME = 'api.rovicorp.com'
 NAMEPATH = 'data/v1/name'
 MUSICPATH = 'search/v2/music'
+F = 'search/v2/music'
 
 def get_artist(id):
-    params = {}
-    params['id'] = id
+    params = []
+    params.append(('id', id))
+
     return get_rovi_response(NAMEPATH, 'info', params)
 
 def get_artist_by_name(name):
-    params = {}
-    params['name'] = name
+    params = []
+    params.append(('name', name))
     return get_rovi_response(NAMEPATH, 'info', params)
 
 
 def get_autocomplete(query):
-    params = {}
-    params['query'] = query
-    params['entitytype'] = 'artist'
-    params['size'] = 10
+    params = []
+    if isinstance(query, unicode):
+        query = query.encode('utf-8')
+    params.append(('query', query))
+    params.append(('entitytype', 'artist'))
+    params.append(('size', 10))
     return get_rovi_response(MUSICPATH, 'autocomplete', params)
 
-def get_rovi_response(path, method, param_dict):
-    param_dict['apikey'] = apikey()
-    param_dict['sig'] = sign()
-    param_dict['format'] = 'json'
-    param_list = []
-
-    for key,val in param_dict.iteritems():
-        if isinstance(val, list):
-            param_list.extend( [(key,subval) for subval in val] )
-        elif val is not None:
-            if isinstance(val, unicode):
-                val = val.encode('utf-8')
-            param_list.append( (key,val) )
+def get_rovi_response(path, method, param_list):
+    param_list.append(('apikey', apikey()))
+    param_list.append(('sig', sign()))
+    param_list.append(('format', 'json'))
 
     params = urllib.urlencode(param_list)
     
