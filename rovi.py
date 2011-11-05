08af5ec967180973452dd6b7f55d7719338a4661
Brian Fife <bdfife@gmail.com>
2011-11-05 18:24:50 -0400

    Added a function to retrieve number of pages for christmas_filterbrowse

--- rovi.py
+++ rovi.py
@@ -58,6 +58,13 @@ def get_filterbrowse_christmas(params):
     response = get_rovi_response(MUSICPATH, 'filterbrowse', params)
     return response["searchResponse"]["results"]
 
+def get_filterbrowse_christmas_pages():
+    params = []
+    params.append(('filter', 'subgenreid:MA0000011929'))
+    params.append(('entitytype', 'album'))
+    response = get_rovi_response(MUSICPATH, 'filterbrowse', params)
+    return response["searchResponse"]["totalResultCounts"]
+
 def get_album_styles(id):   
     params = []
     params.append(('include', "styles"))
